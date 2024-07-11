b9bfe8bed0b1f884f22bcb64927e18ff7770d08f
Felipe Dornelles <lipe.dornelles@hotmail.com>
2024-07-11 07:53:44 -0300

    inicio da tela de nova transação

--- pages/transactions/insert_transaction.py
+++ pages/transactions/insert_transaction.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import streamlit as st
 
 from functions.configs import valid_user_logged
@@ -10,3 +12,26 @@ transactions_service = TransactionsService()
 dados_service = DadosService()
 
 st.header("Adicionar Nova Transação", divider=True)
+
+form = st.form("new_transaction", clear_on_submit=True)
+
+ativo = form.text_input("Ativo")
+qtd = form.number_input(
+    "Quantidade",  value=None, min_value=0)
+price = form.number_input("Preço em R$", value=None, min_value=0)
+date = form.date_input(
+    "Data da Compra", datetime.now().date(), format="DD/MM/YYYY")
+
+submmited = form.form_submit_button("Adicionar Transação")
+if submmited:
+    transaction = {
+        "ativo": ativo,
+        "qtd": qtd,
+        "price": price,
+        "date": date.strftime("%x"),
+        "user_id": st.session_state["user"]["_id"]
+    }
+    transactions_service.insert_transaction(transaction)
+
+    st.write("Transação adicionada")
+    # st.session_state["dados"] = None
