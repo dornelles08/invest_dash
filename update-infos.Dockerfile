b2ad09d47dcc94a4a6fc4f23419d6ff50bbc973c
Felipe Dornelles <lipe.dornelles@hotmail.com>
2024-07-15 16:50:27 -0300

    Ajustei no dockerfile

--- update-infos.Dockerfile
+++ update-infos.Dockerfile
@@ -4,12 +4,8 @@ WORKDIR /app
 
 COPY requirements.txt .
 
-RUN pip install virtualenv
-
-RUN virtualenv .venv
-
-RUN . .venv/bin/activate && pip install -r requirements.txt
+RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["/bin/bash", "-c", ". .venv/bin/activate && python update-infos.py"]
+CMD ["/bin/bash", "-c", "python update-infos.py"]
--- update-infos.py
+++ update-infos.py
@@ -4,24 +4,22 @@ from datetime import datetime
 import schedule
 
 from functions.get_fii_details import get_fii
-from functions.load_data import load_fiis
 from services.dados import DadosService
+from services.transaction import TransactionsService
 from services.user import UserService
 
 
 def job():
     user_services = UserService()
     dados_service = DadosService()
+    transaction_service = TransactionsService()
 
     users = user_services.get_users()
 
     for user in users:
         print(user["name"])
-        if (datetime.strptime(user["sync"], "%x %X") - datetime.now()).seconds > 900:
-            print("Synced")
-            continue
 
-        _, _, fiis, _ = load_fiis(user["_id"])
+        fiis = transaction_service.get_fiis_from_transactions(user["_id"])
         fiis_details = []
         for _, fii in enumerate(fiis):
             data = get_fii(fii)
@@ -32,9 +30,12 @@ def job():
             **user,
             "sync": datetime.now().strftime("%x %X")
         })
+        print("Dados Sincronizados")
 
 
-schedule.every(3).hours.do(job)
+print("Inicio")
+
+schedule.every(15).minutes.do(job)
 
 while True:
     schedule.run_pending()
